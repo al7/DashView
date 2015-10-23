94106a23943c23fd13629325e2a7fb13c9dde8ba
Alex Leite <admin@al7dev.com>
2015-10-22 21:32:08 -0700

    Initial Commit

---
+++ DashView/Source/DashView.swift
@@ -0,0 +1,56 @@
+/*
+
+Copyright (c) 2015 - Alex Leite (al7dev)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+import UIKit
+
+@IBDesignable public class DashView: UIView {
+    @IBInspectable public var dashWidth: CGFloat = 3.0
+    @IBInspectable public var dashSpace: CGFloat = 1.0
+    @IBInspectable public var dashColor: UIColor = UIColor.darkGrayColor()
+    
+    //MARK- Initializers
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clearColor()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.backgroundColor = UIColor.clearColor()
+    }
+    
+    //MARK- Base Overrides
+    
+    public override func drawRect(rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetFillColorWithColor(context, self.dashColor.CGColor)
+        
+        var nextDashFrame = CGRect(x: 0.0, y: 0.0, width: self.dashWidth, height: rect.size.height)
+        while nextDashFrame.origin.x < rect.size.width {
+            CGContextFillRect(context, nextDashFrame)
+            nextDashFrame.origin.x += (self.dashWidth + self.dashSpace)
+        }
+    }
+}
---
+++ DashView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DashView
+//
+//  Created by Alexandre Leite on 10/22/15.
+//  Copyright © 2015 Alexandre Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
